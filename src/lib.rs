8327a66822e426bfb02857c5c7aec6e5a31ac5af
mzdyhrave <info@hravemzdy.org>
2022-03-14 17:09:43 +0100

    Release 0.22.2.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-mod service;
+pub mod service;
+pub mod service_errors;
+pub mod service_types;
+pub mod registry_factories;
+pub mod registry_providers;
 mod service_tests;
 mod registry;
 mod registry_constants;
-mod registry_factories;
-mod registry_providers;
-mod service_errors;
-mod service_types;
 
